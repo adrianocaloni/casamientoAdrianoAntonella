b92763f709d679b7292db16bd0ae1bed562b686b
Adriano Caloni <caloniadrianoj@gmail.com>
2024-01-03 19:35:51 -0300

    prueba de formulario3

--- javascript.js
+++ javascript.js
@@ -1,15 +1,23 @@
 function enviarDatos() {
-  var datos = "adriano,caloni";  // Aquí pones tus datos separados por comas
-  var url = "https://script.google.com/macros/s/AKfycbwRhiuMfTu3RQebj5b-70PgzZ2rTMc3GZFROd_PGU-bjolBMl7hvUM1TKXMAFml1MYlrQ/exec";  // Reemplaza con la URL de tu web app de Google Apps Script
+  var dato1 = document.getElementById("dato1").value;
+  var dato2 = document.getElementById("dato2").value;
+  // Agrega más campos según sea necesario
+
+  var url = "https://script.google.com/macros/s/AKfycbwRhiuMfTu3RQebj5b-70PgzZ2rTMc3GZFROd_PGU-bjolBMl7hvUM1TKXMAFml1MYlrQ/exec";  // Reemplaza con la URL de tu script de Google Apps Script
   fetch(url, {
     method: 'POST',
-    payload: {
-      data: datos
-    }
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      dato1: dato1,
+      dato2: dato2
+      // Agrega más campos según sea necesario
+    })
   })
   .then(response => response.text())
   .then(data => {
-    console.log(data);  // Aquí puedes manejar la respuesta
+    console.log(data);  // Maneja la respuesta del servidor
   })
   .catch(error => {
     console.error('Error:', error);
