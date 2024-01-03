2c9811b6bc63b7b59069c3eb5bd4a9b5c1cc9a59
Adriano Caloni <caloniadrianoj@gmail.com>
2024-01-03 19:27:58 -0300

    prueba de formulario2

--- javascript.js
+++ javascript.js
@@ -1,24 +1,17 @@
-function doPost(e) {
-    var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-    var newRow = sheet.getLastRow() + 1;
-    sheet.getRange(newRow, 1, 1, e.parameter.dato1.split(",").length).setValues([[e.parameter.dato1, e.parameter.dato2]]);
-    return ContentService.createTextOutput("Datos enviados con éxito");
-  }
-
 function enviarDatos() {
-    var datos = "dato1,dato2,dato3";  // Aquí pones tus datos separados por comas
-    var url = "https://docs.google.com/spreadsheets/d/1tOygYuCg-F78Rv8jSrjki7rD0igoQCcz3uhc9gRl3JY/edit?pli=1#gid=0";  // Reemplaza con la URL de tu web app de Google Apps Script
-    fetch(url, {
-      method: 'POST',
-      payload: {
-        data: datos
-      }
-    })
-    .then(response => response.text())
-    .then(data => {
-      console.log(data);  // Aquí puedes manejar la respuesta
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-  }
+  var datos = "dato1,dato2,dato3";  // Aquí pones tus datos separados por comas
+  var url = "https://script.google.com/macros/s/AKfycbwRhiuMfTu3RQebj5b-70PgzZ2rTMc3GZFROd_PGU-bjolBMl7hvUM1TKXMAFml1MYlrQ/exec";  // Reemplaza con la URL de tu web app de Google Apps Script
+  fetch(url, {
+    method: 'POST',
+    payload: {
+      data: datos
+    }
+  })
+  .then(response => response.text())
+  .then(data => {
+    console.log(data);  // Aquí puedes manejar la respuesta
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
