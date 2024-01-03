cde549fd35a0112ea17b6308f980e8da3b123ba6
Adriano Caloni <caloniadrianoj@gmail.com>
2024-01-03 20:33:12 -0300

    prueba de formulario4

--- javascript.js
+++ javascript.js
@@ -3,23 +3,23 @@ function enviarDatos() {
   var dato2 = document.getElementById("dato2").value;
   // Agrega más campos según sea necesario
 
-  var url = "https://script.google.com/macros/s/AKfycbwRhiuMfTu3RQebj5b-70PgzZ2rTMc3GZFROd_PGU-bjolBMl7hvUM1TKXMAFml1MYlrQ/exec";  // Reemplaza con la URL de tu script de Google Apps Script
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      dato1: dato1,
-      dato2: dato2
-      // Agrega más campos según sea necesario
-    })
-  })
-  .then(response => response.text())
-  .then(data => {
-    console.log(data);  // Maneja la respuesta del servidor
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+  var data = {
+    nombre : dato1,
+    apellido: dato2
+  };
+
+  console.log(data)
+
+  var url = "https://script.google.com/macros/s/AKfycbxYl1OU-MrNi3E21LhoAG6WLTJ_v60KaId6JJAW0TOVghLKaPaxLIMcpfpNI0fjFoVyXA/exec";  // Reemplaza con la URL de tu script de Google Apps Script
+  var script = document.createElement('script');
+  script.src = url + '?callback=handleResponse&data=' + encodeURIComponent(JSON.stringify(data));
+  document.body.appendChild(script);
+
+  console.log(script)
+
+}
+
+function handleResponse(response) {
+  console.log(response);
+  // Tu lógica aquí para manejar la respuesta
 }
