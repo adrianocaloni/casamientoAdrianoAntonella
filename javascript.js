265f9a78c623e151d35adcf297be2fd4d8d3d990
Adriano Caloni <caloniadrianoj@gmail.com>
2024-01-03 19:33:03 -0300

    prueba de formulario3

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function enviarDatos() {
-  var datos = "dato1,dato2,dato3";  // Aquí pones tus datos separados por comas
+  var datos = "adriano,caloni";  // Aquí pones tus datos separados por comas
   var url = "https://script.google.com/macros/s/AKfycbwRhiuMfTu3RQebj5b-70PgzZ2rTMc3GZFROd_PGU-bjolBMl7hvUM1TKXMAFml1MYlrQ/exec";  // Reemplaza con la URL de tu web app de Google Apps Script
   fetch(url, {
     method: 'POST',
